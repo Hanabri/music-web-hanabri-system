4c1bc93d204374850de944d0c9af72d7ac03a9bd
Hanabri <g313756670117@outlook.com>
2025-04-04 16:30:41 +0800

    环节配置

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // 使用 process.env.PORT 获取端口号，默认值为 3000
+  const port = process.env.PORT || 3000;
+  // 监听所有网络接口
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
