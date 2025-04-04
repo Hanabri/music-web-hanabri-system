8a99ba3d26a041e1c266c70b68c262d92c69ed61
Hanabri <g313756670117@outlook.com>
2025-04-04 16:41:48 +0800

    环节配置

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // 使用 process.env.PORT 获取端口号，默认值为 3000
+
+  // 添加CORS支持
+  app.enableCors();
+
+  // 显式获取环境变量
   const port = process.env.PORT || 3000;
-  // 监听所有网络接口
-  await app.listen(port, '0.0.0.0');
+  const host = '0.0.0.0';
+
+  await app.listen(port, host, () => {
+    new Logger('NestApplication').log(
+      `服务已启动在 http://${host}:${port}`
+    );
+  });
 }
 bootstrap();
